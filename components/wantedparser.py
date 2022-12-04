59739157917e6bc6fd29d3dec703e9c8020e6a8d
leejaerim <ger6608@naver.com>
2022-12-05 00:27:24 +0900

    [FED-2] 원티드 Parser - wanted get list of job id

--- components/wantedparser.py
+++ components/wantedparser.py
@@ -1,3 +1,5 @@
+import bs4
+
 from .parser import Parser
 import requests
 import json
@@ -9,7 +11,25 @@ class WantedParser(Parser):
         self.url = url
 
     def _parse(self) -> str:
-        html = requests.get(self.url)
-        soup = BeautifulSoup(html.text, "html.parser")
-        # jo = json.loads(soup.find("script"))
-        return soup.find("script").text
+        list_id = self.get_list_of_id()
+        return str(list_id)
+    def get_list_of_id(self)->list:
+        offset = 0
+        limit = 100
+        res = []
+        flag = True
+        while (flag):
+            html = requests.get(self.url + f"&limit=100&offset={offset}")
+            print(self.url + f"&limit=100&offset={offset}")
+            soup = BeautifulSoup(html.text, "html.parser")
+            jo = json.loads(soup.text)
+            if jo["data"] is None:
+                flag = False
+            else:
+                for i in jo["data"]:
+                    res.append(i["id"])
+                offset += limit
+        return res
+
+
+
--- main.py
+++ main.py
@@ -7,6 +7,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-
-    html = WantedParser("https://www.wanted.co.kr/wd/138471").parse()
+    html = WantedParser("https://www.wanted.co.kr/api/v4/jobs?1670145066301&country=kr&tag_type_ids=518&job_sort=company.response_rate_order&locations=all&years=-1").parse()
     return HTMLResponse(content=html, status_code=200)
